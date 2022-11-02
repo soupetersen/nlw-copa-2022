46aef8eaac519707c4647d1368adbc1ade5697a3
soupetersen <soupetersen@gmail.com>
2022-11-02 19:34:02 -0300

    Add google authentication and context

---
+++ mobile/src/contexts/AuthContext.tsx
@@ -0,0 +1,71 @@
+import { createContext, useState, useEffect } from "react";
+import * as Google from "expo-auth-session/providers/google";
+import * as AuthSession from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
+import { GOOGLE_CLIENT_ID } from "@env";
+
+WebBrowser.maybeCompleteAuthSession();
+
+export interface UserProps {
+  id: string;
+  name: string;
+  email: string;
+  avatarUrl?: string;
+}
+
+export interface AuthContextDataProps {
+  user: UserProps;
+  isUserLoading: boolean;
+  signIn: () => Promise<void>;
+}
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<UserProps>({} as UserProps);
+  const [isUserLoading, setIsUserLoading] = useState(false);
+
+  const  [ request, response, promptAsync ] = Google.useAuthRequest({
+    clientId: GOOGLE_CLIENT_ID,
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ["profile", "email"],
+  });
+
+  async function signIn() {
+    try {
+      setIsUserLoading(true);
+      await promptAsync();
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
+  }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log("Token de Autenticação", access_token);
+  }
+
+  useEffect(() => {
+    if (response?.type === "success" && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
+
+  return (
+    <AuthContext.Provider 
+    value={{ 
+        user: user, 
+        isUserLoading,
+        signIn 
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
