b5441657e9c5e04d696956c09e8c267025dc6251
soupetersen <soupetersen@gmail.com>
2022-11-02 19:34:53 -0300

    Some changes in mobile config files

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -10,6 +10,12 @@ import { NativeBaseProvider, StatusBar } from 'native-base';
 import { THEME } from "./src/styles/theme";
 import { Loading } from './src/components/Loading';
 import { SignIn } from './src/screens/SignIn';
+import { AuthContextProvider } from "./src/contexts/AuthContext";
+import { New } from "./src/screens/New";
+import { Find } from "./src/screens/find";
+import { Pools } from "./src/screens/Pools";
+
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,12 +26,14 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <SignIn/> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Pools/> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
