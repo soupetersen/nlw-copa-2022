02bac1101dc07907b2c841481ab21785e34d389d
soupetersen <soupetersen@gmail.com>
2022-11-02 13:39:12 -0300

    Add prisma migrations and routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import { PrismaClient } from "@prisma/client"
 import cors from "@fastify/cors";
+import { z } from "zod";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
     log: ['query']
@@ -18,6 +20,36 @@ async function bootstrap() {
     fastify.get('/pools/count', async (request, reply) => {
         const count = await prisma.pool.count();
         return {count};
+    }); 
+
+    fastify.get('/users/count', async (request, reply) => {
+        const count = await prisma.user.count();
+        return {count};
+    }); 
+
+    fastify.get('/guesses/count', async (request, reply) => {
+        const count = await prisma.guess.count();
+        return {count};
+    }); 
+
+    fastify.post('/pools', async (request, reply) => {
+        const createPoolBody = z.object({
+            title: z.string(),
+        });
+
+        const { title } = createPoolBody.parse(request.body);
+
+        const generate = new ShortUniqueId({ length: 6 });
+        const code = String(generate()).toUpperCase();
+
+        const pool = await prisma.pool.create({
+            data: {
+                title,
+                code: code,
+            }
+        });
+
+        return reply.code(201).send({ code });
     }); 
     
    await fastify.listen({ port: 3333,  });
